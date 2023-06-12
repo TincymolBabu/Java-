ca34826c3175317e7323cdd4efb537b9e674b5e0
TincymolBabu <117815480+TincymolBabu@users.noreply.github.com>
2023-06-12 15:15:58 +0530

    Add files via upload

---
+++ Lab Cycle2/student.java
@@ -0,0 +1,27 @@
+
+import java.util.Scanner;
+public class student
+{
+    public static void main(String[] args) 
+    {
+        int n, total = 0, percentage;
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter no. of subject:");
+        n = s.nextInt();
+        int marks[] = new int[n];
+        System.out.println("Enter marks out of 100:");
+        for(int i = 0; i < n; i++)
+        {   
+            marks[i] = s.nextInt();
+            while(marks[i]>100)
+            {
+            System.out.println("invalid");
+            marks[i] = s.nextInt();
+            }
+            total = total + marks[i];
+        }
+        percentage = total / n;
+        System.out.println("Total marks:"+total);
+        System.out.println("Percentage:"+percentage +"%");
+    }
+}
